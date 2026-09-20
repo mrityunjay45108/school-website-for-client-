7565ad5e6389d86abf8ed3fc02501f8d98302e42
mrityunjay45108 <kumarmrityunjay5210@gmail.com>
2026-09-20 12:26:48 +0530

    fix: import PrismaModule in TopperModule

--- backend/src/topper/topper.module.ts
+++ backend/src/topper/topper.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TopperService } from './topper.service.js';
 import { TopperController } from './topper.controller.js';
+import { PrismaModule } from '../prisma/prisma.module.js';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [TopperController],
   providers: [TopperService],
 })
