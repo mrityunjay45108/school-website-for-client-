8d1ff1fa7b1268279c9db96640a7551625f35cbd
mrityunjay45108 <kumarmrityunjay5210@gmail.com>
2026-09-20 12:25:44 +0530

    feat: secure topper write endpoints

--- backend/src/topper/topper.controller.ts
+++ backend/src/topper/topper.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { TopperService } from './topper.service.js';
 import { CreateTopperDto } from './dto/create-topper.dto.js';
 import { UpdateTopperDto } from './dto/update-topper.dto.js';
@@ -7,6 +8,7 @@ import { UpdateTopperDto } from './dto/update-topper.dto.js';
 export class TopperController {
   constructor(private readonly topperService: TopperService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   create(@Body() createTopperDto: CreateTopperDto) {
     return this.topperService.create(createTopperDto);
@@ -22,11 +24,13 @@ export class TopperController {
     return this.topperService.findOne(id);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTopperDto: UpdateTopperDto) {
     return this.topperService.update(id, updateTopperDto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.topperService.remove(id);
